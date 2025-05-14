19afee530ebac991c05ecb7f860e904d7703bfb4
aimdotsh <liups@liups.com>
2025-05-14 10:10:04 +0800

    Update usePrivacyMode.ts

--- src/hooks/usePrivacyMode.ts
+++ src/hooks/usePrivacyMode.ts
@@ -1,6 +1,9 @@
 export const usePrivacyMode = () => {
-  return {
-    isPrivacyMode: true,
-    setIsPrivacyMode: () => {},
+  // 始终返回隐私模式开启状态
+  const isPrivacyMode = true;
+  const setIsPrivacyMode = () => {
+    console.log('Privacy mode is always enabled');
   };
+
+  return { isPrivacyMode, setIsPrivacyMode };
 };
