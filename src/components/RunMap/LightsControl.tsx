3278dfc0ae532ae813973eb90fb35f60a52bca63
aimdotsh <liups@liups.com>
2025-05-15 11:23:31 +0800

    隐私模式按钮背景移除

--- src/components/RunMap/LightsControl.tsx
+++ src/components/RunMap/LightsControl.tsx
@@ -7,11 +7,7 @@ interface ILightsProps {
 
 // 移除了隐私模式控制按钮，保留组件结构以避免引用错误
 const LightsControl = ({ setLights, lights }: ILightsProps) => {
-  return (
-    <div className={"mapboxgl-ctrl mapboxgl-ctrl-group " + styles.lights}>
-      {/* 隐私模式按钮已移除 */}
-    </div>
-  );
+  return null;
 };
 
 export default LightsControl;
