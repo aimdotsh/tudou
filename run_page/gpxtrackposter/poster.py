80286c7c4a9ca5ad5a837d217f2dd0ae642012a5
aimdotsh <liups@liups.com>
2025-05-25 10:13:34 +0800

    Update poster.py

--- run_page/gpxtrackposter/poster.py
+++ run_page/gpxtrackposter/poster.py
@@ -147,13 +147,13 @@ class Poster:
 
     def __draw_header(self, d):
         text_color = self.colors["text"]
-        title_style = "font-size:9px; font-family:Arial; font-weight:bold;"
+        title_style = "font-size:6px; font-family:Arial; font-weight:bold;"
         d.add(d.text(self.title, insert=(10, 20), fill=text_color, style=title_style))
 
     def __draw_footer(self, d):
         text_color = self.colors["text"]
         header_style = "font-size:4px; font-family:Arial"
-        value_style = "font-size:9px; font-family:Arial"
+        value_style = "font-size:6px; font-family:Arial"
         small_value_style = "font-size:3px; font-family:Arial"
 
         special_distance1 = self.special_distance["special_distance"]
