71f7cb8cc23278f68fba73566556deedc12f8539
aimdotsh <liups@liups.com>
2026-01-04 10:55:37 +0800

    修正svg

--- assets/index.tsx
+++ assets/index.tsx
@@ -1,5 +1,5 @@
 export const yearStats = import.meta.glob('./year_*.svg', { import: 'ReactComponent' })
-export const totalStat = import.meta.glob(['./github.svg', './grid.svg', './mol.svg'], { import: 'ReactComponent' })
+export const totalStat = import.meta.glob(['./github.svg', './grid.svg', './mol.svg', './ayeartotal_*.svg'], { import: 'ReactComponent' })
 export const recentStat = import.meta.glob(['./yyyymmdd/*.svg'], { import: 'ReactComponent' })
 export const halfmarathonStat = import.meta.glob(['./halfmarathon/*.svg'], { import: 'ReactComponent' })
 export const newyearStat = import.meta.glob(['./newyear/*.svg'], { import: 'ReactComponent' })
